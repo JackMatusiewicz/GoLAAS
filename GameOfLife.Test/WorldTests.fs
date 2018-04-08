82d5ed81ab449240d7a2bca6c19d6e31cc0f1cf4
JackMatusiewicz <jack.matusiewicz@gmail.com>
2018-04-08 15:32:49 +0100

    Adding basic world creation tests

---
+++ GameOfLife.Test/WorldTests.fs
@@ -0,0 +1,36 @@
+﻿namespace GameOfLife.Test
+
+open GameOfLife
+open NUnit.Framework
+
+module WorldTests =
+
+    [<Test>]
+    let ``Given live cells with negative coordinates, when world is create, then none is returned`` () =
+        let aliveCells =
+            [
+                (-1, 0)
+                (5,10)
+                (15,20)
+                (0,0)
+                (20,20)
+            ] |> Set.ofList
+        let dimensions = (21u, 21u)
+
+        let world = World.make dimensions aliveCells
+        Assert.That(world, Is.EqualTo(None))
+
+    [<Test>]
+    let ``Given live cells with coordinates out of range, when world is create, then none is returned`` () =
+        let aliveCells =
+            [
+                (0, 0)
+                (5,10)
+                (15,20)
+                (0,0)
+                (21,20)
+            ] |> Set.ofList
+        let dimensions = (21u, 21u)
+
+        let world = World.make dimensions aliveCells
+        Assert.That(world, Is.EqualTo(None))
